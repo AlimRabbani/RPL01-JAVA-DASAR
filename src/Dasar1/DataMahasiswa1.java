38699d163c52ace19ed749cbfdcaad9717dfeb23
AlimRabbani <alimmanurung18@gmail.com>
2022-10-17 18:02:38 +0800

    Day 01

---
+++ src/Dasar1/DataMahasiswa1.java
@@ -0,0 +1,16 @@
+package Dasar1;
+
+public class DataMahasiswa1 {
+
+    public static void main(String[] args) {
+        String nama = "alim rabbani";
+        int umur = 19;
+        boolean status = (true);
+
+        System.out.println("nama saya :" + nama);
+        System.out.println("umur :" + umur);
+        System.out.println(" staus mahasiswa aktif:" + status);
+
+    }
+
+}
---
+++ src/Dasar1/HellowWord1.java
@@ -0,0 +1,10 @@
+package Dasar1;
+ 
+public class HellowWord1 { 
+    public static void main(String[] args) {
+        System.out.println("Hellow word");  
+        System.out.println(" Hellow code"); 
+        System.out.println("Hellow semuanya");
+    }
+    
+}
